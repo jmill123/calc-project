6c1b419f7abf61f5a5f8290e4e25b81156186669
jmill123 <170875141+jmill123@users.noreply.github.com>
2024-09-04 23:41:02 -0400

    Add JS to Add Chars to Screen, made related tweaks

---
+++ script.js
@@ -0,0 +1,36 @@
+//Function Definitions
+function operate(currentOp) {
+    if(currentOp === "+") {
+        num3 = num1 + num2;
+    }
+    else if(currentOp === "-") {
+        num3 = num1 - num2;
+    }
+    else if(currentOp === "/") {
+        num3 = num1 / num2;
+    }
+    else if(currentOp === "X") {
+        num3 = num1 * num2;
+    }
+    return num3;
+
+}
+
+
+function addToDisplay(char) {
+screen.textContent = screen.textContent + " " + char;
+}
+
+
+function clearDisplay() {
+screen.textContent = '';
+}
+
+//Initialize Vars and Selectors
+let num1
+let num2
+let currentOp
+
+const screen = document.querySelector(".screen");
+
+//Running Code
